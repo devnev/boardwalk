e3c65cb7bb172f8fc38a86c62877af9319a1b9d5
Mark Nevill <mark.nevill@gmail.com>
2016-08-21 22:04:17 +0100

    Make external interface clearer using underscores.

--- index.jsx
+++ index.jsx
@@ -9,22 +9,22 @@ class App extends React.Component {
         duration: 1*60*60
       }
     };
-    this.updateRange = this.updateRange.bind(this);
-    this.navigate = this.navigate.bind(this);
+    this._updateRange = this._updateRange.bind(this);
+    this._navigate = this._navigate.bind(this);
   }
   componentDidMount() {
     this.req = $.get("/config.json");
     this.req.done(function(data) { this.setState({config: data}); }.bind(this));
-    window.addEventListener("hashchange", this.navigate);
-    this.navigate();
+    window.addEventListener("hashchange", this._navigate);
+    this._navigate();
   }
   componentWillUnmount() {
     if (this.req) {
       this.req.abort();
     }
-    window.removeEventListener("hashchange", this.navigate);
+    window.removeEventListener("hashchange", this._navigate);
   }
-  updateRange(range) {
+  _updateRange(range) {
     if (range.end.getTime() == this.state.range.end.getTime() &&
         range.duration == this.state.range.duration) {
       return;
@@ -35,7 +35,7 @@ class App extends React.Component {
       range: range
     });
   }
-  navigate() {
+  _navigate() {
     var path = window.location.hash.substr(1);
     var paramsStart = path.indexOf("?");
     if (paramsStart != -1) {
@@ -56,8 +56,8 @@ class App extends React.Component {
       <div>
         <ConsoleNav consoles={this.state.config} />
         <h1>{console.title}</h1>
-        <RangePicker range={this.state.range} onChange={this.updateRange} />
-        <Console key={path} items={console.contents} range={this.state.range} onChangeRange={this.updateRange} />
+        <RangePicker range={this.state.range} onChange={this._updateRange} />
+        <Console key={path} items={console.contents} range={this.state.range} onChangeRange={this._updateRange} />
       </div>
     );
   }
@@ -86,7 +86,7 @@ class Console extends React.Component {
     this.tScale = new Plottable.Scales.Time();
     this.focusPoint = new Plottable.Dataset();
     if (props.range) {
-      this.setRange(props.range);
+      this._setRange(props.range);
     }
     this.tScale.onUpdate(function() {
       this.props.onChangeRange({
@@ -95,12 +95,12 @@ class Console extends React.Component {
       });
     }.bind(this));
   }
-  setRange(range) {
+  _setRange(range) {
     this.tScale.domain([new Date(range.end.getTime() - range.duration*1000), range.end]);
   }
   shouldComponentUpdate(nextProps, nextState) {
     if (_.isEqual(this.props.items, nextProps.items)) {
-      this.setRange(nextProps.range);
+      this._setRange(nextProps.range);
       return false;
     }
     return true;
@@ -261,9 +261,9 @@ class Query {
   }
   _onFocusPointChanged(dataset) {
     var data = [undefined].concat(dataset.data());
-    this.updateNearest(data.pop());
+    this._updateNearest(data.pop());
   }
-  updateNearest(targetDate) {
+  _updateNearest(targetDate) {
     if (!targetDate) {
       this.nearest.data([]);
       return;
@@ -349,13 +349,13 @@ class Graph extends React.Component {
     pointer.attachTo(panel);
 
     // binds
-    this.redraw = this.redraw.bind(this);
-    this.onParamsUpdate = _.debounce(this.onParamsUpdate.bind(this), 500);
+    this._redraw = this._redraw.bind(this);
+    this._onParamsUpdate = _.debounce(this._onParamsUpdate.bind(this), 500);
   }
-  redraw() {
+  _redraw() {
     this.chart.redraw();
   }
-  onParamsUpdate() {
+  _onParamsUpdate() {
     var start = Math.floor(this.props.tScale.domainMin().getTime()/1000);
     var end = Math.floor(this.props.tScale.domainMax().getTime()/1000);
     var step = Math.floor((end - start) / 100).toString() + "s";
@@ -365,9 +365,9 @@ class Graph extends React.Component {
       }
       this.loading.req.abort();
     }
-    this.updateData();
+    this._updateData();
   }
-  updateData() {
+  _updateData() {
     var start = Math.floor(this.props.tScale.domainMin().getTime()/1000);
     var end = Math.floor(this.props.tScale.domainMax().getTime()/1000);
     this.queries.forEach(function(query) {
@@ -375,16 +375,16 @@ class Graph extends React.Component {
     });
   }
   componentDidMount() {
-    this.updateData();
-    window.addEventListener("resize", this.redraw);
-    this.props.tScale.onUpdate(this.onParamsUpdate);
+    this._updateData();
+    window.addEventListener("resize", this._redraw);
+    this.props.tScale.onUpdate(this._onParamsUpdate);
   }
   componentWillUnmount() {
     if (this.req) {
       this.req.abort();
     }
-    window.removeEventListener("resize", this.redraw);
-    this.props.tScale.offUpdate(this.onParamsUpdate);
+    window.removeEventListener("resize", this._redraw);
+    this.props.tScale.offUpdate(this._onParamsUpdate);
   }
   render() {
     return <svg id={this.id} width="100%" height="300px" ref={(ref) => this.chart.renderTo(ref)} />
@@ -399,17 +399,17 @@ Graph.propTypes = {
 class RangePicker extends React.Component {
   constructor(props) {
     super(props);
-    this.updateDuration = this.updateDuration.bind(this);
-    this.updateEnd = this.updateEnd.bind(this);
+    this._updateDuration = this._updateDuration.bind(this);
+    this._updateEnd = this._updateEnd.bind(this);
   }
-  updateDuration(duration) {
+  _updateDuration(duration) {
     this.props.onChange({end: this.props.range.end, duration: duration});
   }
-  updateEnd(end) {
+  _updateEnd(end) {
     this.props.onChange({end: end, duration: this.props.range.duration});
   }
   render() {
-    return <div><DurationPicker value={this.props.range.duration} onChange={this.updateDuration} /><TimePicker value={this.props.range.end} step={this.props.range.duration} onChange={this.updateEnd} /></div>
+    return <div><DurationPicker value={this.props.range.duration} onChange={this._updateDuration} /><TimePicker value={this.props.range.end} step={this.props.range.duration} onChange={this._updateEnd} /></div>
   }
 }
 RangePicker.propTypes = {
@@ -472,10 +472,10 @@ class DurationPicker extends React.Component {
       inputValue: DurationPicker.formatDuration(props.value),
       dirty: false
     }
-    this.onIncreaseDuration = this.onIncreaseDuration.bind(this);
-    this.onDecreaseDuration = this.onDecreaseDuration.bind(this);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.onInputChange = this.onInputChange.bind(this);
+    this._onIncreaseDuration = this._onIncreaseDuration.bind(this);
+    this._onDecreaseDuration = this._onDecreaseDuration.bind(this);
+    this._onFormSubmit = this._onFormSubmit.bind(this);
+    this._onInputChange = this._onInputChange.bind(this);
   }
   componentWillReceiveProps(nextProps) {
     var nextDuration = DurationPicker.formatDuration(nextProps.value)
@@ -483,7 +483,7 @@ class DurationPicker extends React.Component {
       this.setState({ inputValue: nextDuration, dirty: false });
     }
   }
-  onIncreaseDuration() {
+  _onIncreaseDuration() {
     for (var i = 0; i < this.steps.length; i++) {
       if (this.steps[i] > this.props.value) {
         this.props.onChange(this.steps[i]);
@@ -492,7 +492,7 @@ class DurationPicker extends React.Component {
     }
     this.props.onChange(this.props.value * 2);
   }
-  onDecreaseDuration() {
+  _onDecreaseDuration() {
     for (var i = this.steps.length; i > 0; i--) {
       if (this.steps[i-1] < this.props.value) {
         this.props.onChange(this.steps[i-1]);
@@ -500,22 +500,22 @@ class DurationPicker extends React.Component {
       }
     }
   }
-  onFormSubmit(event) {
+  _onFormSubmit(event) {
     event.preventDefault();
     var duration = DurationPicker.parseDuration(this.state.inputValue);
     if (duration != 0) {
       this.props.onChange(duration)
     }
   }
-  onInputChange(event) {
+  _onInputChange(event) {
     this.setState({inputValue: event.target.value, dirty: true });
   }
   render() {
     return (
-      <form action="" onSubmit={this.onFormSubmit}>
-        <button type="button" onClick={this.onDecreaseDuration}>&#8722;</button>
-        <input type="text" value={this.state.inputValue} onChange={this.onInputChange} className={(DurationPicker.parseDuration(this.state.inputValue) == 0 ? "error" : "valid") + (this.state.dirty ? " dirty" : "")} />
-        <button type="button" onClick={this.onIncreaseDuration}>+</button>
+      <form action="" onSubmit={this._onFormSubmit}>
+        <button type="button" onClick={this._onDecreaseDuration}>&#8722;</button>
+        <input type="text" value={this.state.inputValue} onChange={this._onInputChange} className={(DurationPicker.parseDuration(this.state.inputValue) == 0 ? "error" : "valid") + (this.state.dirty ? " dirty" : "")} />
+        <button type="button" onClick={this._onIncreaseDuration}>+</button>
       </form>
     );
   }
@@ -532,13 +532,13 @@ class TimePicker extends React.Component {
       inputValue: moment(this.props.value).format("YYYY-MM-DD HH:mm:ssZZ"),
       dirty: false,
     }
-    this.onInputChange = this.onInputChange.bind(this);
-    this.onPickNow = this.onPickNow.bind(this);
-    this.onStepBack = this.onStepBack.bind(this);
-    this.onStepForward = this.onStepForward.bind(this);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this._onInputChange = this._onInputChange.bind(this);
+    this._onPickNow = this._onPickNow.bind(this);
+    this._onStepBack = this._onStepBack.bind(this);
+    this._onStepForward = this._onStepForward.bind(this);
+    this._onFormSubmit = this._onFormSubmit.bind(this);
   }
-  parsedInput() {
+  _parsedInput() {
     return moment(this.state.inputValue, moment.ISO_8601, true);
   }
   componentWillReceiveProps(nextProps) {
@@ -547,32 +547,32 @@ class TimePicker extends React.Component {
       this.setState({ inputValue: nextValue, dirty: false });
     }
   }
-  onInputChange(event) {
+  _onInputChange(event) {
     this.setState({inputValue: event.target.value, dirty: true});
   }
-  onPickNow() {
+  _onPickNow() {
     this.props.onChange(new Date());
   }
-  onStepBack() {
+  _onStepBack() {
     this.props.onChange(new Date(this.props.value.getTime() - this.props.step*1000));
   }
-  onStepForward() {
+  _onStepForward() {
     this.props.onChange(new Date(this.props.value.getTime() + this.props.step*1000));
   }
-  onFormSubmit(event) {
+  _onFormSubmit(event) {
     event.preventDefault();
-    var value = this.parsedInput();
+    var value = this._parsedInput();
     if (value.isValid()) {
       this.props.onChange(value.toDate());
     }
   }
   render() {
     return (
-      <form action="" onSubmit={this.onFormSubmit}>
-        <button type="button" onClick={this.onStepBack}>-{DurationPicker.formatDuration(this.props.step)}</button>
-        <input type="text" value={this.state.inputValue} onChange={this.onInputChange} className={(this.parsedInput().isValid() ? "valid" : "error") + (this.state.dirty ? " dirty" : "")}/>
-        <button type="button" onClick={this.onStepForward}>+{DurationPicker.formatDuration(this.props.step)}</button>
-        <button type="button" onClick={this.onPickNow}>now</button>
+      <form action="" onSubmit={this._onFormSubmit}>
+        <button type="button" onClick={this._onStepBack}>-{DurationPicker.formatDuration(this.props.step)}</button>
+        <input type="text" value={this.state.inputValue} onChange={this._onInputChange} className={(this._parsedInput().isValid() ? "valid" : "error") + (this.state.dirty ? " dirty" : "")}/>
+        <button type="button" onClick={this._onStepForward}>+{DurationPicker.formatDuration(this.props.step)}</button>
+        <button type="button" onClick={this._onPickNow}>now</button>
       </form>
     );
   }
