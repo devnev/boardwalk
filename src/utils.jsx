980feb575fd4d49a4242f9ebcf1a8fdda990ce8b
Mark Nevill <mark.nevill@gmail.com>
2016-09-03 12:47:01 +0100

    Add small file license headers.

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,3 +1,6 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+
 import _ from 'underscore';
 
 export function FormatMetric(metric) {
