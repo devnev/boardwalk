8c21a711472e41597494bf83fc541ab506129f2e
Mark Nevill <mark.nevill@gmail.com>
2016-09-20 00:29:22 +0100

    Indent case statements in switches.

--- src/reducers.js
+++ src/reducers.js
@@ -10,114 +10,118 @@ const filterPrefix = "filter.";
 
 function consoleReducer(state = "", action) {
   switch (action.type) {
-  case LOCATION_CHANGE: {
-    if (!action.payload) {
-      return '';
+    case LOCATION_CHANGE: {
+      if (!action.payload) {
+        return '';
+      }
+      let {pathname} = action.payload;
+      return pathname.replace(/\/+$/, "");
     }
-    let {pathname} = action.payload;
-    return pathname.replace(/\/+$/, "");
-  }
-  case 'SELECT_CONSOLE':
-    return action.console;
+    case 'SELECT_CONSOLE': {
+      return action.console;
+    }
   }
   return state;
 }
 
 function configReducer(state = null, action) {
   switch (action.type) {
-  case 'RECEIVE_CONFIG':
-    return action.config;
+    case 'RECEIVE_CONFIG': {
+      return action.config;
+    }
   }
   return state;
 }
 
 function rangeReducer(state = {duration: null, end: null}, action) {
   switch (action.type) {
-  case 'INITIALIZE': {
-    // note reverse order of Object.assign - intentional
-    return Object.assign({}, action.range, state);
-  }
-  case LOCATION_CHANGE: {
-    if (!action.payload) {
-      return {duration: 60*60, end: action.now};
+    case 'INITIALIZE': {
+      // note reverse order of Object.assign - intentional
+      return Object.assign({}, action.range, state);
     }
-    let params = querystring.parse(action.payload.search);
-    let range = Object.assign({}, state, {duration: 60*60, end: action.now});
-    if (/^\d+$/.test(params['duration'])) {
-      range.duration = Number(params['duration']);
+    case LOCATION_CHANGE: {
+      if (!action.payload) {
+        return {duration: 60*60, end: action.now};
+      }
+      let params = querystring.parse(action.payload.search);
+      let range = Object.assign({}, state, {duration: 60*60, end: action.now});
+      if (/^\d+$/.test(params['duration'])) {
+        range.duration = Number(params['duration']);
+      }
+      if (/^\d+$/.test(params['end'])) {
+        range.end = new Date(Number(params['end']*1000));
+      }
+      if (!_.isEqual(range, state)) {
+        return range;
+      }
+      break;
     }
-    if (/^\d+$/.test(params['end'])) {
-      range.end = new Date(Number(params['end']*1000));
+    case 'MODIFY_TIME_SCALE': {
+      let {start, end} = action;
+      let duration = Math.round((end.getTime() - start.getTime())/1000);
+      let range = {duration, end};
+      if (!_.isEqual(state, range)) {
+        return range;
+      }
+      break;
     }
-    if (!_.isEqual(range, state)) {
-      return range;
+    case 'PICK_DURATION': {
+      if (!_.isEqual(state.duration, action.duration)) {
+        return Object.assign({}, state, { duration: action.duration });
+      }
+      break;
     }
-    break;
-  }
-  case 'MODIFY_TIME_SCALE': {
-    let {start, end} = action;
-    let duration = Math.round((end.getTime() - start.getTime())/1000);
-    let range = {duration, end};
-    if (!_.isEqual(state, range)) {
-      return range;
+    case 'PICK_END': {
+      if (!_.isEqual(state.end, action.end)) {
+        return Object.assign({}, state, { end: action.end });
+      }
+      break;
     }
-    break;
-  }
-  case 'PICK_DURATION':
-    if (!_.isEqual(state.duration, action.duration)) {
-      return Object.assign({}, state, { duration: action.duration });
-    }
-    break;
-  case 'PICK_END':
-    if (!_.isEqual(state.end, action.end)) {
-      return Object.assign({}, state, { end: action.end });
-    }
-    break;
   }
   return state;
 }
 
 function filterReducer(state = {}, action) {
   switch (action.type) {
-  case LOCATION_CHANGE: {
-    if (!action.payload) {
-      return {};
-    }
-    let params = querystring.parse(action.payload.search);
-    let filter = {};
-    _.each(params, (values, key) => {
-      if (key.startsWith(filterPrefix)) {
-        let v = Array.isArray(values) ? values[values.length-1] : values;
-        filter[key.substr(filterPrefix.length)] = v;
+    case LOCATION_CHANGE: {
+      if (!action.payload) {
+        return {};
       }
-    });
-    if (!_.isEqual(state, filter)) {
-      return filter;
-    }
-    break;
-  }
-  case 'SET_FILTERS': {
-    let filters = Object.assign({}, state);
-    _.each(action.filters, (value, name) => {
-      if (value) {
-        filters[name] = value;
-      } else {
-        delete filters[name];
+      let params = querystring.parse(action.payload.search);
+      let filter = {};
+      _.each(params, (values, key) => {
+        if (key.startsWith(filterPrefix)) {
+          let v = Array.isArray(values) ? values[values.length-1] : values;
+          filter[key.substr(filterPrefix.length)] = v;
+        }
+      });
+      if (!_.isEqual(state, filter)) {
+        return filter;
       }
-    });
-    if (!_.isEqual(filters, state)) {
-      return filters;
+      break;
     }
-    break;
-  }
+    case 'SET_FILTERS': {
+      let filters = Object.assign({}, state);
+      _.each(action.filters, (value, name) => {
+        if (value) {
+          filters[name] = value;
+        } else {
+          delete filters[name];
+        }
+      });
+      if (!_.isEqual(filters, state)) {
+        return filters;
+      }
+      break;
+    }
   }
   return state;
 }
 
 function hoverReducer(state, action) {
   switch (action.type) {
-  case 'HOVER':
-    return {time: action.time};
+    case 'HOVER':
+      return {time: action.time};
   }
   return state ? state : {time: action.now};
 }
@@ -126,18 +130,22 @@ const initialExpandedState = { panelIndex: null, queryIndex: null, metricLabels:
 
 function expandedReducer(state = initialExpandedState, action) {
   switch (action.type) {
-  case 'SELECT_CONSOLE':
-    return Object.assign({}, initialExpandedState);
-  case 'SET_FILTERS':
-    return Object.assign({}, initialExpandedState);
-  case 'EXPAND_METRIC':
-    return {
-      panelIndex: action.panelIndex,
-      queryIndex: action.queryIndex,
-      metricLabels: action.metricLabels,
-    };
-  case 'COLLAPSE_METRIC':
-    return Object.assign({}, initialExpandedState);
+    case 'SELECT_CONSOLE': {
+      return Object.assign({}, initialExpandedState);
+    }
+    case 'SET_FILTERS': {
+      return Object.assign({}, initialExpandedState);
+    }
+    case 'EXPAND_METRIC': {
+      return {
+        panelIndex: action.panelIndex,
+        queryIndex: action.queryIndex,
+        metricLabels: action.metricLabels,
+      };
+    }
+    case 'COLLAPSE_METRIC': {
+      return Object.assign({}, initialExpandedState);
+    }
   }
   return state;
 }
@@ -171,48 +179,50 @@ function parseQueryParams(location) {
 
 function routerReducer(state = { locationBeforeTransitions: null }, action) {
   switch (action.type) {
-  case 'INITIALIZE': {
-    let {end, duration} = action.range;
-    let qs = parseQueryParams(state.locationBeforeTransitions);
-    return setQueryParams(state, {
-      duration: qs.duration || duration,
-      end: qs.end || Math.round(end.getTime()/1000),
-    });
-  }
-  case LOCATION_CHANGE:
-    return Object.assign({}, state, { locationBeforeTransitions: action.payload });
-  case 'SELECT_CONSOLE':
-    return {
-      ...state,
-      locationBeforeTransitions: {
-        ...state.locationBeforeTransitions,
-        pathname: action.console,
-      },
-    };
-  case 'MODIFY_TIME_SCALE': {
-    let {start, end} = action;
-    let duration = Math.round((end.getTime() - start.getTime())/1000);
-    return setQueryParams(state, {duration, end: Math.round(end.getTime()/1000)});
-  }
-  case 'PICK_DURATION': {
-    return setQueryParams(state, {duration: action.duration});
-  }
-  case 'PICK_END': {
-    return setQueryParams(state, {end: Math.round(action.end.getTime()/1000)});
-  }
-  case 'SET_FILTERS': {
-    let filters = action.filters;
-    let qsFilters = {};
-    _.each(filters, (value, name) => {
-      var key = filterPrefix + name;
-      if (value) {
-        qsFilters[key] = value;
-      } else {
-        qsFilters[key] = undefined;
-      }
-    });
-    return setQueryParams(state, qsFilters);
-  }
+    case 'INITIALIZE': {
+      let {end, duration} = action.range;
+      let qs = parseQueryParams(state.locationBeforeTransitions);
+      return setQueryParams(state, {
+        duration: qs.duration || duration,
+        end: qs.end || Math.round(end.getTime()/1000),
+      });
+    }
+    case LOCATION_CHANGE: {
+      return Object.assign({}, state, { locationBeforeTransitions: action.payload });
+    }
+    case 'SELECT_CONSOLE': {
+      return {
+        ...state,
+        locationBeforeTransitions: {
+          ...state.locationBeforeTransitions,
+          pathname: action.console,
+        },
+      };
+    }
+    case 'MODIFY_TIME_SCALE': {
+      let {start, end} = action;
+      let duration = Math.round((end.getTime() - start.getTime())/1000);
+      return setQueryParams(state, {duration, end: Math.round(end.getTime()/1000)});
+    }
+    case 'PICK_DURATION': {
+      return setQueryParams(state, {duration: action.duration});
+    }
+    case 'PICK_END': {
+      return setQueryParams(state, {end: Math.round(action.end.getTime()/1000)});
+    }
+    case 'SET_FILTERS': {
+      let filters = action.filters;
+      let qsFilters = {};
+      _.each(filters, (value, name) => {
+        var key = filterPrefix + name;
+        if (value) {
+          qsFilters[key] = value;
+        } else {
+          qsFilters[key] = undefined;
+        }
+      });
+      return setQueryParams(state, qsFilters);
+    }
   }
   return state;
 }
