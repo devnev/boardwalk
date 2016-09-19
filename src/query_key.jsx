ed1fd869aeccc9e5f028825a4f09efb341924539
Mark Nevill <mark.nevill@gmail.com>
2016-09-19 22:27:28 +0100

    Allow expanding queries by clicking on labels.

--- src/query_key.jsx
+++ src/query_key.jsx
@@ -7,11 +7,14 @@ export class QueryKey extends React.Component {
   render() {
     const renderItem = (item, index) => {
       const color = this.context.colorScale.scale(item.title);
+      const select = () => this.props.onSelectMetric(item.queryIndex, item.metric);
       return (
         <li key={index+"-"+item.title}>
-          <span style={{color: color}}>&#x25cf;</span>
-          <span>{item.title}</span>
-          <span>{item.value}</span>
+          <a onClick={select}>
+            <span style={{color: color}}>&#x25cf;</span>
+            <span>{item.title}</span>
+            <span>{item.value}</span>
+          </a>
         </li>
       );
     };
